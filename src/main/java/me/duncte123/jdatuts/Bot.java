79ae9226f30535afafc61e659a3380079c139b61
duncte123 <duncan.sterken123@gmail.com>
2020-03-21 16:18:12 +0100

    New stuff yay

--- src/main/java/me/duncte123/jdatuts/Bot.java
+++ src/main/java/me/duncte123/jdatuts/Bot.java
@@ -6,8 +6,11 @@ import me.duncte123.jdatuts.database.DatabaseManager;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
+import java.util.EnumSet;
 
 public class Bot {
 
@@ -21,8 +24,17 @@ public class Bot {
                 .setFooter("MeuDocs Tutorials")
         );
 
-        new JDABuilder()
-                .setToken(Config.get("token"))
+        JDABuilder.createDefault(
+                Config.get("token"),
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_MESSAGES
+        )
+                .setDisabledCacheFlags(EnumSet.of(
+                        CacheFlag.CLIENT_STATUS,
+                        CacheFlag.ACTIVITY,
+                        CacheFlag.EMOTE,
+                        CacheFlag.VOICE_STATE
+                ))
                 .addEventListeners(new Listener())
                 .setActivity(Activity.watching("MenuDocs"))
                 .build();
