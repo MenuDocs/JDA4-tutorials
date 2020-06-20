1825236ed3756e4d8c3b0f4ae166817d839927df
duncte123 <contact@duncte123.me>
2020-06-20 11:33:16 +0200

    Episode 19

---
+++ src/main/java/me/duncte123/jdatuts/lavaplayer/TrackScheduler.java
@@ -0,0 +1,36 @@
+package me.duncte123.jdatuts.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.event.AudioEventAdapter;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TrackScheduler extends AudioEventAdapter {
+    private final AudioPlayer player;
+    private final BlockingQueue<AudioTrack> queue;
+
+    public TrackScheduler(AudioPlayer player) {
+        this.player = player;
+        this.queue = new LinkedBlockingQueue<>();
+    }
+
+    public void queue(AudioTrack track) {
+        if (!this.player.startTrack(track, true)) {
+            this.queue.offer(track);
+        }
+    }
+
+    public void nextTrack() {
+        this.player.startTrack(this.queue.poll(), false);
+    }
+
+    @Override
+    public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
+        if (endReason.mayStartNext) {
+            nextTrack();
+        }
+    }
+}
