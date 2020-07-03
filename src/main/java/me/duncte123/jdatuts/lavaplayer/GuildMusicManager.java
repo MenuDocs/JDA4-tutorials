c7cb6b3bd9a319a533c4b6c9efc44ef01579d504
duncte123 <contact@duncte123.me>
2020-07-03 11:11:59 +0200

    Talk about stretching episodes

---
+++ src/main/java/me/duncte123/jdatuts/lavaplayer/GuildMusicManager.java
@@ -0,0 +1,23 @@
+package me.duncte123.jdatuts.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+
+public class GuildMusicManager {
+    public final AudioPlayer audioPlayer;
+
+    public final TrackScheduler scheduler;
+
+    private final AudioPlayerSendHandler sendHandler;
+
+    public GuildMusicManager(AudioPlayerManager manager) {
+        this.audioPlayer = manager.createPlayer();
+        this.scheduler = new TrackScheduler(this.audioPlayer);
+        this.audioPlayer.addListener(this.scheduler);
+        this.sendHandler = new AudioPlayerSendHandler(this.audioPlayer);
+    }
+
+    public AudioPlayerSendHandler getSendHandler() {
+        return sendHandler;
+    }
+}
---
+++ src/main/java/me/duncte123/jdatuts/lavaplayer/PlayerManager.java
@@ -0,0 +1,80 @@
+package me.duncte123.jdatuts.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerManager {
+    private static PlayerManager INSTANCE;
+
+    private final Map<Long, GuildMusicManager> musicManagers;
+    private final AudioPlayerManager audioPlayerManager;
+
+    public PlayerManager() {
+        this.musicManagers = new HashMap<>();
+        this.audioPlayerManager = new DefaultAudioPlayerManager();
+
+        AudioSourceManagers.registerRemoteSources(this.audioPlayerManager);
+        AudioSourceManagers.registerLocalSource(this.audioPlayerManager);
+    }
+
+    public GuildMusicManager getMusicManager(Guild guild) {
+        return this.musicManagers.computeIfAbsent(guild.getIdLong(), (guildId) -> {
+            final GuildMusicManager guildMusicManager = new GuildMusicManager(this.audioPlayerManager);
+
+            guild.getAudioManager().setSendingHandler(guildMusicManager.getSendHandler());
+
+            return guildMusicManager;
+        });
+    }
+
+    public void loadAndPlay(TextChannel channel, String trackUrl) {
+        final GuildMusicManager musicManager = this.getMusicManager(channel.getGuild());
+
+        this.audioPlayerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
+            @Override
+            public void trackLoaded(AudioTrack track) {
+                musicManager.scheduler.queue(track);
+
+                channel.sendMessage("Adding to queue: `")
+                        .append(track.getInfo().title)
+                        .append("` by `")
+                        .append(track.getInfo().author)
+                        .queue();
+            }
+
+            @Override
+            public void playlistLoaded(AudioPlaylist playlist) {
+                //
+            }
+
+            @Override
+            public void noMatches() {
+                //
+            }
+
+            @Override
+            public void loadFailed(FriendlyException exception) {
+                //
+            }
+        });
+    }
+
+    private static PlayerManager getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new PlayerManager();
+        }
+
+        return INSTANCE;
+    }
+
+}
