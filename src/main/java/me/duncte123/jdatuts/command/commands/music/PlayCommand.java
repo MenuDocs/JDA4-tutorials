f12a9dec393e953fc668127cb9403a74228816c3
duncte123 <contact@duncte123.me>
2020-07-12 11:56:42 +0200

    Add play command

---
+++ src/main/java/me/duncte123/jdatuts/command/commands/music/PlayCommand.java
@@ -0,0 +1,50 @@
+package me.duncte123.jdatuts.command.commands.music;
+
+import me.duncte123.jdatuts.command.CommandContext;
+import me.duncte123.jdatuts.command.ICommand;
+import me.duncte123.jdatuts.lavaplayer.PlayerManager;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+public class PlayCommand implements ICommand {
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public void handle(CommandContext ctx) {
+        final TextChannel channel = ctx.getChannel();
+        final Member self = ctx.getSelfMember();
+        final GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        if (!selfVoiceState.inVoiceChannel()) {
+            channel.sendMessage("I need to be in a voice channel for this to work").queue();
+            return;
+        }
+
+        final Member member = ctx.getMember();
+        final GuildVoiceState memberVoiceState = member.getVoiceState();
+
+        if (!memberVoiceState.inVoiceChannel()) {
+            channel.sendMessage("You need to be in a voice channel for this command to work").queue();
+            return;
+        }
+
+        if (!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+            channel.sendMessage("You need to be in the same voice channel as me for this to work").queue();
+            return;
+        }
+
+        PlayerManager.getInstance()
+                .loadAndPlay(channel, "https://www.youtube.com/watch?v=JiF3pbvR5G0");
+    }
+
+    @Override
+    public String getName() {
+        return "play";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Plays a song\n" +
+                "Usage: `!!play <youtube link>`";
+    }
+}
