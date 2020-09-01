fd25c7ab34dc5a7d3e52d5d9bb3927605e98872b
duncte123 <contact@duncte123.me>
2020-09-01 08:12:11 +0200

    Yes ep 26 already

--- src/main/java/me/duncte123/jdatuts/command/commands/music/NowPlayingCommand.java
+++ src/main/java/me/duncte123/jdatuts/command/commands/music/NowPlayingCommand.java
@@ -1,12 +1,53 @@
 package me.duncte123.jdatuts.command.commands.music;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import me.duncte123.jdatuts.command.CommandContext;
 import me.duncte123.jdatuts.command.ICommand;
+import me.duncte123.jdatuts.lavaplayer.GuildMusicManager;
+import me.duncte123.jdatuts.lavaplayer.PlayerManager;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 public class NowPlayingCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
-        //
+        final TextChannel channel = ctx.getChannel();
+        final Member self = ctx.getSelfMember();
+        final GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        if (!selfVoiceState.inVoiceChannel()) {
+            channel.sendMessage("I need to be in a voice channel for this to work").queue();
+            return;
+        }
+
+        final Member member = ctx.getMember();
+        final GuildVoiceState memberVoiceState = member.getVoiceState();
+
+        if (!memberVoiceState.inVoiceChannel()) {
+            channel.sendMessage("You need to be in a voice channel for this command to work").queue();
+            return;
+        }
+
+        if (!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+            channel.sendMessage("You need to be in the same voice channel as me for this to work").queue();
+            return;
+        }
+
+        final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(ctx.getGuild());
+        final AudioPlayer audioPlayer = musicManager.audioPlayer;
+        final AudioTrack track = audioPlayer.getPlayingTrack();
+
+        if (track == null) {
+            channel.sendMessage("There is no track playing currently").queue();
+            return;
+        }
+
+        final AudioTrackInfo info = track.getInfo();
+
+        channel.sendMessageFormat("Now playing `%s` by `%s` (Link: <%s>)", info.title, info.author, info.uri).queue();
     }
 
     @Override
